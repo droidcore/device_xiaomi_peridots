991f59afd41747cd6b91170da74167fa4916a9a3
droidcore <agusmahardika99@gmail.com>
2025-11-18 10:35:32 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -29,9 +29,6 @@ $(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
 # Viper4AndroidFX
 $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 
-# Lineage-Priv
--include vendor/lineage-priv/keys/keys.mk
-
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
@@ -224,7 +221,6 @@ PRODUCT_DEXPREOPT_SPEED_APPS += \
     Launcher3QuickStep\
     Settings \
     SystemUI
-
 
 # HIDL
 PRODUCT_PACKAGES += \
