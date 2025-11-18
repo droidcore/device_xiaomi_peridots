405b8e96ff86e4ccd55fb952121cb5e893aad74e
droidcore <agusmahardika99@gmail.com>
2025-11-18 13:12:28 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -276,6 +276,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Lineage
+PRODUCT_PACKAGES -= Seedvault
+
 # KProfiles
 PRODUCT_PACKAGES += \
     KProfiles
