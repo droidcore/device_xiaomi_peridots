50f1a99a4407c88c0c5f8fd43996bc77920174ca
droidcore <agusmahardika99@gmail.com>
2025-11-19 20:20:46 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -127,8 +127,8 @@ TARGET_KERNEL_CONFIG := \
 
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
-    swinfo.fingerprint=peridot:$(AOSP_VERSION) \
-    mtdoops.fingerprint=peridot:$(AOSP_VERSION)
+    swinfo.fingerprint=peridot:$(LINEAGE_VERSION) \
+    mtdoops.fingerprint=peridot:$(LINEAGE_VERSION)
 
 BOARD_KERNEL_CMDLINE += rcupdate.rcu_expedited=1 rcu_nocbs=all rcutree.enable_rcu_lazy
 
