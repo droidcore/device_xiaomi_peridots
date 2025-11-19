eba9bd14551b1d08348d9af18fb5218b278565a0
droidcore <agusmahardika99@gmail.com>
2025-11-19 18:37:13 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -188,9 +188,7 @@ PRODUCT_COPY_FILES += \
 # GNSS
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
-    
-# GameBar Performance Overlay
-$(call inherit-product, packages/apps/GameBar/gamebar.mk) 
+
 
 # Health
 PRODUCT_PACKAGES += \
